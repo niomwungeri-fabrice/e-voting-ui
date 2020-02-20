2d96c909a304045a17feff65d338d7e09531cbf2
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2020-02-20 17:44:26 +0200

    add landing page

---
+++ src/components/Smart/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import '../../styles/app.css'
+import Landing from "../Presentational/Landing";
+
+export const App = () => {
+    return (
+        <div className="App">
+            <Landing/>
+        </div>
+    );
+};
+
