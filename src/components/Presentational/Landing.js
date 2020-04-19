34c04b2acd6c5164e2b827195af97c111f5ddd14
niomwungeri-fabrice <niomwungeri.fabrice@gmail.com>
2020-04-19 11:22:58 +0200

    fix centering

--- src/components/Presentational/Landing.js
+++ src/components/Presentational/Landing.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import 'antd/dist/antd.css';
-import '../../styles/landing.css';
+import React from "react";
+import "antd/dist/antd.css";
+import "../../styles/landing.css";
+import landing from "../../assets/img/landing_background.png";
+import { Button } from "antd";
 
 const Landing = () => {
   return (
-    <div>
-      <h1>landing view</h1>
+    <div
+      style={{
+        backgroundImage: `url(${landing})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+      
+    >
+      <div style={{
+        display:'flex',
+        justifyContent: 'space-between'
+      }}>
+      <h1>Tora</h1>
+      <Button>Login</Button>
+      </div>
+      <hr/>
     </div>
   );
 };
