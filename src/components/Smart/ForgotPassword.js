66e732439d025ea8f4eef999fff11646ea89e673
niomwungeri-fabrice <niomwungeri.fabrice@gmail.com>
2020-04-20 16:35:00 +0200

    add forgot password react template

--- src/components/Smart/ForgotPassword.js
+++ src/components/Smart/ForgotPassword.js
@@ -1,10 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Input, Button, Form } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 
 export default class ForgotPassword extends Component {
+  handlePassword = () => {
+    alert("Please check your email");
+  };
   render() {
     return (
-      <div>
-        <h1>Forgot password view</h1>
+      <div className="centered">
+        <Form className="login-form">
+          <div className="text-centered">
+            <h1>TORA</h1>
+            <h2>Administration</h2>
+          </div>
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            className="login-input"
+            size="large"
+            placeholder="Email"
+          />
+          <Button
+            onClick={this.handlePassword}
+            size="large"
+            type="primary"
+            block
+          >
+            Get Reset Link
+          </Button>
+        </Form>
       </div>
     );
   }
